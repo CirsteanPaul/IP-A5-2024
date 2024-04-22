34c444711c7d5d399500b6630dd5fc4b762b063e
FlavianB <flaviburca@gmail.com>
2024-04-22 20:14:33 +0300

    Added basic unit test for delete account operation

--- IPproject/Features/Accounts/DeleteAccount.cs
+++ IPproject/Features/Accounts/DeleteAccount.cs
@@ -3,17 +3,14 @@ using IP.Project.Database;
 using IP.Project.Entities;
 using IP.Project.Shared;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace IP.Project.Features.Accounts;
 
 
 public class DeleteAccount
 {
-    public record Command : IRequest<Result>
-    {
-        public Guid Id { get; set; }
-    }
-
+    public record Command(Guid Id) : IRequest<Result>;
     public class Handler : IRequestHandler<Command, Result>
     {
         private readonly ApplicationDBContext dbContext;
@@ -25,7 +22,7 @@ public class DeleteAccount
 
         public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
         {
-            var accountToBeDeleted = await dbContext.FindAsync<Account>(request.Id);
+            var accountToBeDeleted = await dbContext.Accounts.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
 
             if (accountToBeDeleted is null)
             {
@@ -47,8 +44,7 @@ public class DeleteAccountEndpoint : ICarterModule
     {
         app.MapDelete("api/v1/accounts/{id}", async (Guid id, ISender sender) =>
         {
-            var command = new DeleteAccount.Command() { Id = id };
-
+            var command = new DeleteAccount.Command(id);
             var result = await sender.Send(command);
 
             if (result.IsFailure)
