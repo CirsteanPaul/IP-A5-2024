358892db4b64d3ef07e210053d19a9d3214062ee
FerbeiPatricia <125951654+FerbeiPatricia@users.noreply.github.com>
2024-04-28 15:37:11 +0300

    Integration Testing - tests for delete endpoint

---
+++ IP.Project.IntegrationTests/Controllers/Vpns/VpnDeleteControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using IP.Project.IntegrationTests.Base.TestingBaseWebApplicationFactory;
+using Xunit;
+
+namespace IP.Project.IntegrationTests.Controllers.Vpns
+{
+    public class VpnDeleteControllerTests : IClassFixture<TestingBaseWebApplicationFactory>
+    {
+        private const string RequestUri = "/api/v1/vpns/";
+
+        private readonly TestingBaseWebApplicationFactory factory;
+
+        public VpnDeleteControllerTests(TestingBaseWebApplicationFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task When_DeleteVpn_Exists_Then_Success()
+        {
+            // Arrange
+            var existingVpnId = Guid.Parse("2330d4f5-1c5b-42cb-a34b-d9275e99b6bc");
+
+            // Act
+            var response = await factory.Client.DeleteAsync($"{RequestUri}{existingVpnId}");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task When_DeleteVpn_DoesNotExist_Then_NotFound()
+        {
+            // Arrange
+            var nonExistingVpnId = Guid.Parse("b1f5d163-ee13-411a-4144-07dc5ef3042e");
+
+            // Act
+            var response = await factory.Client.DeleteAsync($"{RequestUri}{nonExistingVpnId}");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+    }
+}
