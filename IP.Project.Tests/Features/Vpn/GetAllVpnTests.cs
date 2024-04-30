1186e727736e7ad6e7be547b3a191b8a0fe2f5ef
GarbiaCosmin <115941639+GarbiaCosmin@users.noreply.github.com>
2024-04-30 09:44:40 +0300

    Add files via upload

---
+++ IP.Project.Tests/Features/Vpn/GetAllVpnTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using IP.Project.Entities;
+using IP.Project.Features.Vpn;
+using IP.Project.Tests.Base;
+
+namespace IP.Project.Tests.Features.Vpn
+{
+    public class GetAllVpnTests : BaseTest<VpnAccount>
+    {
+        [Fact]
+        public async Task GetAllVpnHandler_ReturnsListOfVpnAccounts()
+        {
+            // Arrange
+            var vpnAccounts = new List<VpnAccount>
+            {
+                new VpnAccount { Id = Guid.NewGuid(),Description = "VPN 1", IPv4Address = "192.168.1.1" },
+                new VpnAccount { Id = Guid.NewGuid(), Description = "VPN 2", IPv4Address = "192.168.1.2" }
+            };
+            var mock = Setup(vpnAccounts);
+
+            var sut = new GetAllVpns.Handler(mock);
+            var query = new GetAllVpns.Query();
+
+            // Act
+            var result = await sut.Handle(query, default);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().NotBeEmpty();
+            result.Value.Should().HaveCount(2);
+            result.Value.Should().ContainEquivalentOf(new { Description = "VPN 1", IPv4Address = "192.168.1.1" });
+            result.Value.Should().ContainEquivalentOf(new { Description = "VPN 2", IPv4Address = "192.168.1.2" });
+        }
+
+        [Fact]
+        public async Task GetAllVpnHandler_NoVpnAccounts_ReturnsEmptyList()
+        {
+            // Arrange
+            var mock = Setup(new List<VpnAccount>());
+            var sut = new GetAllVpns.Handler(mock);
+            var query = new GetAllVpns.Query();
+
+            // Act
+            var result = await sut.Handle(query, default);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().BeEmpty();
+        }
+    }
+}
