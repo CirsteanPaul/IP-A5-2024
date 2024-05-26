c1b9d0ed79299696930acae5eafce57ad6935e50
FlavianB <flaviburca@gmail.com>
2024-05-27 00:41:55 +0300

    Added partial entry to ldap server when we encounter a new matricol

--- IPproject/Features/Accounts/GetMailInfo.cs
+++ IPproject/Features/Accounts/GetMailInfo.cs
@@ -1,5 +1,4 @@
-﻿using Azure.Core;
-using Carter;
+﻿using Carter;
 using IP.Project.Contracts;
 using IP.Project.Database;
 using IP.Project.Entities;
@@ -7,15 +6,10 @@ using IP.Project.Features.Accounts;
 using IP.Project.Shared;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration.EnvironmentVariables;
-using Microsoft.OpenApi.Any;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Numerics;
-using System.Threading;
+using System.Reflection.PortableExecutable;
+using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
 
 namespace IP.Project.Features.Accounts
 {
@@ -37,6 +31,7 @@ namespace IP.Project.Features.Accounts
 
             public async Task<Result<int>> addPartialEntryToDb(string matricol, CancellationToken cancellationToken)
             {
+                //add partial entry to db
                 var uidNumber = await generateUidNumber(cancellationToken);
 
                 var account = new Account
@@ -50,8 +45,8 @@ namespace IP.Project.Features.Accounts
                     sn = "Popescu", // from esims
                     gidNumber = uidNumber, // same value as uidNumber, uniquely generated between 2000 and 7999
                     uidNumber = uidNumber, // same value as gidNumber, uniquely generated between 2000 and 7999
-                    uid = "", // part of email
-                    homeDirectory = "", // /home/username(uid)
+                    uid = "to.be", // part of email
+                    homeDirectory = "/home/to.be", // /home/username(uid)
                     displayName = "Ion Popescu", // from esims
                     employeeNumber = "",
                     givenName = "Ion", // from esims
@@ -63,7 +58,7 @@ namespace IP.Project.Features.Accounts
                     ou = "1A1", // from esims
                     postalCode = "",
                     roomNumber = "",
-                    shadowInactive = "",
+                    shadowInactive = "0",
                     street = "",
                     telephoneNumber = "",
                     title = "",
@@ -74,10 +69,90 @@ namespace IP.Project.Features.Accounts
 
                 account.initials = account.givenName[0] + account.sn[0].ToString();
 
-
-
                 dbContext.Accounts.Add(account);
                 await dbContext.SaveChangesAsync(cancellationToken);
+
+                //add partial entry to ldap
+                string ldapServer = "LDAP://localhost:10389";
+                string userName = "uid=admin, ou=system"; // admin user
+                string password = "secret";
+
+                var directoryEntry = new System.DirectoryServices.DirectoryEntry(ldapServer, userName, password, AuthenticationTypes.ServerBind);
+                directoryEntry.Path = "LDAP://localhost:10389/ou=license,ou=students,dc=info,dc=uaic,dc=ro";
+
+                try
+                {
+                    // Authenticate the admin
+                    object nativeObject = directoryEntry.NativeObject;
+                    Console.WriteLine("Authenticated");
+
+                    //Create a new organizational unit if necessary for the student group of the user
+                    DirectorySearcher searcher = new DirectorySearcher(directoryEntry)
+                    {
+                        PageSize = int.MaxValue,
+                        Filter = "(&(objectClass=organizationalUnit)(ou=" + account.ou +"))"
+                    };
+
+                    var result = searcher.FindOne();
+
+                    if (result == null)
+                    {
+                        // Create a new organizational unit entry
+                        System.DirectoryServices.DirectoryEntry newOU = directoryEntry.Children.Add("ou=" + account.ou, "organizationalUnit");
+                        newOU.Properties["objectClass"].Add("organizationalUnit");
+
+                        // Save the new organizational unit entry
+                        newOU.CommitChanges();
+                        Console.WriteLine("Organizational unit added successfully.");
+                    }
+
+
+                    // Create a new user entry
+                    directoryEntry.Path = "LDAP://localhost:10389/ou=" + account.ou + ",ou=license, ou=students, dc=info, dc=uaic, dc=ro";
+                    System.DirectoryServices.DirectoryEntry newUser = directoryEntry.Children.Add("cn=" + account.displayName, "inetOrgPerson");
+
+                    // Set user attributes
+                    newUser.Properties["objectClass"].Add("inetOrgPerson");
+                    newUser.Properties["objectClass"].Add("organizationalPerson");
+                    newUser.Properties["objectClass"].Add("person");
+                    newUser.Properties["objectClass"].Add("posixAccount");
+                    newUser.Properties["objectClass"].Add("shadowAccount");
+
+
+                    // Null properties cannot be set, but they will be pulled from ESIMS
+                    // uid and homeDirectory are mandatory because of posixAccount objectClass
+                    // sn and cn are mandatory because of inetOrgPerson objectClass
+                    newUser.Properties["sn"].Add(account.sn);
+                    //newUser.Properties["mail"].Add(account.mail);  
+                    newUser.Properties["uidNumber"].Add(account.uidNumber); 
+                    newUser.Properties["gidNumber"].Add(account.gidNumber); 
+                    newUser.Properties["uid"].Add(account.uid);
+                    newUser.Properties["homeDirectory"].Add(account.homeDirectory);
+                    newUser.Properties["displayName"].Add(account.displayName);
+                    //newUser.Properties["employeeNumber"].Add(account.employeeNumber);
+                    newUser.Properties["givenName"].Add(account.givenName);
+                    //newUser.Properties["homePhone"].Add(account.homePhone);
+                    newUser.Properties["initials"].Add(account.initials);
+                    newUser.Properties["localityName"].Add(account.localityName);
+                    newUser.Properties["localityName"].Add("Suceava");
+                    //newUser.Properties["mobile"].Add(account.mobile);
+                    newUser.Properties["ou"].Add(account.ou);
+                    //newUser.Properties["postalCode"].Add(account.postalCode);
+                    //newUser.Properties["roomNumber"].Add(account.roomNumber);
+                    //newUser.Properties["shadowInactive"].Add(account.shadowInactive);
+                    //newUser.Properties["street"].Add(account.street);
+                    //newUser.Properties["telephoneNumber"].Add(account.telephoneNumber);
+                    //newUser.Properties["title"].Add(account.title);
+                    //newUser.Properties["userPassword"].Add(account.userPassword);
+
+                    // Save the new user entry
+                    newUser.CommitChanges();
+                    Console.WriteLine("User added successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("LDAP" + ex.Message);
+                }
 
                 return account.uidNumber;
             }
@@ -226,7 +301,7 @@ public class GetMailInfoEndpoint : ICarterModule
             return Results.Ok(result.Value);
         }).WithTags("Accounts")
         .WithDescription("Endpoint for getting mail variants by user information. " +
-        "If the request succeeds, in the response body you can find the mail address.")
+        "If the request succeeds, in the response body you can find the mail addresses.")
         .Produces<AccountResponse>(StatusCodes.Status200OK)
         .Produces<Error>(StatusCodes.Status404NotFound)
         .WithOpenApi();
